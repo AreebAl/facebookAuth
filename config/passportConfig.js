84bb63d6dac4372281fcd6e5433367589246da52
AreebAl <areebcalm@gmail.com>
2025-01-07 16:30:26 +0530

    callback url config

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
       profileFields: ['id', 'displayName', 'emails', 'photos'],
     },
     (accessToken, refreshToken, profile, done) => {
